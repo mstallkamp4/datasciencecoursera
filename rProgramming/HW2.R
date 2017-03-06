24e8be3fd5b7b0274e65b368f374a7694ba503db
mstallkamp4 <mstallkamp@gmail.com>
2017-03-05 23:57:26 -0500

    Updated HW2

--- rProgramming/HW2.R
+++ rProgramming/HW2.R
@@ -48,6 +48,71 @@ mean(bindedset, na.rm=TRUE)
 test <-pollutantmean ("specdata", "nitrate", 23)
 test
 
-##1.682
+complete <-function(directory, id=1:322) {
+    bindedset <-data.frame(id=numeric(), nobs=numeric())
+    #print(bindedset)
+    for(i in id) 
+    {
+        
+        filename <- paste("00",i, collapse="", sep="" )
+        filename <-substr(filename, nchar(filename)-2, nchar(filename))
+        filenamefull <- paste(directory, "\\", filename, ".csv", collapse="", sep="")
+        
+        input <- read.csv(filenamefull)
+        ##Read one csv to find out how to get complete records
+        #input <- read.csv("specdata\\001.csv")
+        #input
+        ##Find the set where sulfate is not na
+        goodsulfates <-!is.na(input[2])
+        inputwithgoodsulfates <-(input[goodsulfates,])
+        inputwithgoodsulfates
+        goodnitrates <-!is.na(inputwithgoodsulfates[3])
+        goodrecords <-inputwithgoodsulfates[goodnitrates,]
+        #print(i)
+        nobdf=data.frame(id=i, nobs=nrow(goodrecords))
+        bindedset<-rbind.data.frame(nobdf, bindedset)
+        #print(nobdf)
+    }
+   
+     bindedset
+}
+complete("specdata", c(2,4,8,10,12))
 
-?cbind
+
+
+
+corr <-function(directory, threshold=0) {
+    bindedset <-data.frame(nitrate=numeric(), sulfate=numeric())
+    #print(bindedset)
+    for(i in 1:323) 
+    {
+        
+        filename <- paste("00",i, collapse="", sep="" )
+        filename <-substr(filename, nchar(filename)-2, nchar(filename))
+        filenamefull <- paste(directory, "\\", filename, ".csv", collapse="", sep="")
+        
+        input <- read.csv(filenamefull)
+        ##Read one csv to find out how to get complete records
+        #input <- read.csv("specdata\\001.csv")
+        #input
+        ##Find the set where sulfate is not na
+        goodsulfates <-!is.na(input[2])
+        inputwithgoodsulfates <-(input[goodsulfates,])
+        inputwithgoodsulfates
+        goodnitrates <-!is.na(inputwithgoodsulfates[3])
+        goodrecords <-inputwithgoodsulfates[goodnitrates,]
+        #print(i)
+        nobdf=data.frame(id=i, nobs=nrow(goodrecords))
+        dfgood=data.frame(nitrate=goodrecords[2], sulfate=goodrecords[3])
+        
+        if (nobdf$nobs > threshold)
+        {
+        bindedset<-rbind.data.frame(dfgood, bindedset)
+        }
+        y <- nrow(bindedset)
+    }
+    print (y)
+    cor(bindedset)
+}
+cr<-corr("specdata")
+length(cr)
